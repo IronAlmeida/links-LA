971688bce96f4a580c4a921f5d1ef4d14ca8a50f
Iron Almeida <54252720+IronAlmeida@users.noreply.github.com>
2024-04-26 13:39:22 +0000

    Links LA adicionados

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
+  const html = document.documentElement;
+  html.classList.toggle("light");
   //pegar tag imagem
-  const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img");
 
   //substituir imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar imagem light mode
-    img.setAttribute("src", "./assets/perfil-light.png")
+    img.setAttribute("src", "./assets/perfil-light.png");
     img.setAttribute(
       "alt",
       "Foto de Iron Almeida sorrindo com fundo cinza e oculos escuro."
-    )
+    );
   } else {
     //se tiver dark mode, mante imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png");
     img.setAttribute(
       "alt",
       "Foto de Iron Almeida sorrindo, usando oculos, de barba e fundo cinza."
-    )
+    );
   }
 }
 
-let data = new Date()
-const anoAtual = data.getFullYear()
+let data = new Date();
+const anoAtual = data.getFullYear();
 
-document.getElementById("footer").innerHTML = `Iron Almeida - Desenvolvedor Web Full Stack | ${anoAtual}`
+document.getElementById(
+  "footer"
+).innerHTML = `Lourdes Almeida - LA Consultoria e Treinamento | ${anoAtual}`;
